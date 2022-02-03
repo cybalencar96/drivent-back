b89a169f2a49db15dd3e25ad2ab74db15d493173
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2022-02-03 11:12:53 -0300

    created route for certificate

---
+++ src/controllers/client/certificate.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import fs from "fs";
+import * as service from "@/services/client/certificate";
+
+async function getCertificate(req: Request, res: Response) {
+  service.createCertificate();
+  res.contentType("applicaton/pdf");
+  fs.createReadStream("./output.pdf").pipe(res);
+}
+
+export { getCertificate };
--- src/routers/client/index.ts
+++ src/routers/client/index.ts
@@ -8,6 +8,7 @@ import enrollmentRouter from "@/routers/client/enrollment";
 import hotelRouter from "@/routers/client/hotel";
 import roomRouter from "@/routers/client/room";
 import reservationRouter from "@/routers/client/reservation";
+import certificateRouter from "@/routers/client/certificate";
 
 import tokenValidationMiddleware from "@/middlewares/tokenValidationMiddleware";
 
@@ -21,5 +22,6 @@ router.use("/ticket", tokenValidationMiddleware, ticketRouter);
 router.use("/reservation", reservationRouter);
 router.use("/hotel", tokenValidationMiddleware, hotelRouter);
 router.use("/room", tokenValidationMiddleware, roomRouter);
+router.use("/certificate", certificateRouter);
 
 export default router;
