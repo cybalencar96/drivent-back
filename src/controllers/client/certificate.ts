3a9b89efea7f560c7447af899341563025df6f97
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2022-02-03 16:52:55 -0300

    corrected pdf extension

--- src/controllers/client/certificate.ts
+++ src/controllers/client/certificate.ts
@@ -5,6 +5,7 @@ import * as service from "@/services/client/certificate";
 async function getCertificate(req: Request, res: Response) {
   service.createCertificate();
   res.contentType("applicaton/pdf");
+  res.attachment("certificate.pdf");
   fs.createReadStream("./output.pdf").pipe(res);
 }
 
